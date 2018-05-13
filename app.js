1aa14400e52d8e9e7833ca7fe2b096fac446d56f
fabioharry <38741153+fabioharry@users.noreply.github.com>
2018-05-13 20:07:50 -0300

    Adição de interação entre os clientes e servidor usando websocket

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var server = app.listen(80, function(){
  /* Fazendo com que o websocket tambem fique ouvindo na porta 80 */
 var io = require('socket.io').listen(server)
 
+/* Criando uma variavel global usando o express*/
+app.set('io', io)
+
  /* Criar a conexão por websocket */
  io.on('connection', function(socket){
      console.log('Usuário conectou')
@@ -16,4 +19,32 @@ var io = require('socket.io').listen(server)
      socket.on('disconnect', function(){
          console.log('Usuário desconectou')
      })
+
+     socket.on('msgParaServidor', function(data){
+         /* Dialogo */
+        /* Faz a mensagem que enviou aparecer na propria tela do chat */
+        socket.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        )
+        /* Faz a mensagem que enviou aparecer nas demais tela do chat, menos de quem enviou */
+        socket.broadcast.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        )
+
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            )
+            /* Faz a mensagem que enviou aparecer nas demais tela do chat, menos de quem enviou */
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            )
+        }
+     })
+
  })
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -13,6 +13,12 @@ module.exports.iniciaChat = function(application, req, res){
         res.render("index", { validacao : erros })
         return
     }
+    
+    application.get('io').emit(
+        'msgParaCliente',
+        {apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+    
+    )
 
-    res.render("chat")
+    res.render("chat", {dadosForm : dadosForm})
 }
