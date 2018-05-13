71d1f80d05507f3552ca680bcf4c024a253b0d9b
fabioharry <38741153+fabioharry@users.noreply.github.com>
2018-05-12 23:17:22 -0300

    Configuração do Websocket.io

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 var app = require('./config/server')
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online')
 })
+
+ /* Fazendo com que o websocket tambem fique ouvindo na porta 80 */
+var io = require('socket.io').listen(server)
+
+ /* Criar a conexão por websocket */
+ io.on('connection', function(socket){
+     console.log('Usuário conectou')
+
+     socket.on('disconnect', function(){
+         console.log('Usuário desconectou')
+     })
+ })
